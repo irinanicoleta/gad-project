27ccda7899bcd283f074d5916b886329f4069e65
Irina <irinan.ungureanu@gmail.com>
2021-02-27 20:53:54 +0200

    Add some comments and small final changes

--- calendarproject/calendarapp/urls.py
+++ calendarproject/calendarapp/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'calendarapp'
 urlpatterns = [
+    # url pentru calendar (pagina principala)
     url(r'^calendar/$', views.ViewCalendar.as_view(), name='calendar'),
+    # url pentru un nou eveniment
     url(r'^event/new/$', views.change_event, name='new_event'),
+    # url pentru editarea unui eveniment existent
     url(r'^event/(?P<event_id>\d+)/$', views.change_event, name='edit_event'),
 ]
